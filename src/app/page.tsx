f4d8344194bd40116d68718af6a35183070d5ca4
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2023-11-23 22:00:19 +0700

    create home sreen page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Sreen from "@/components/molekul/screen";
+
 export default function Home() {
   return (
-    <div className=" flex px-1 flex-col min-h-screen items-center  bg-[#308CFB]"></div>
+    <div className=" flex px-1 flex-col min-h-screen justify-center items-center overflow-hidden bg-[#308CFB]">
+      <Sreen />
+    </div>
   );
 }
---
+++ src/components/molekul/screen/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Sreen = () => {
+  return (
+    <>
+      <div className=" flex flex-col">
+        <div className=" flex justify-center relative -top-[50px] w-full h-8/12">
+          <div className=" flex justify-center items-center w-[554px] h-[554px] rounded-full border-2 border-white ">
+            <div className=" flex justify-center items-center w-[356px] h-[356px] rounded-full border-2 border-white">
+              <Image
+                src={"/logo-2.png"}
+                alt={"logo"}
+                width={100}
+                height={100}
+                className=" w-full h-full p-24"
+              />
+            </div>
+          </div>
+        </div>
+        <div className=" -mt-1 flex flex-col items-center justify-center font-mulis text-white ">
+          <p className=" text-lg">Selamat datang di</p>
+          <p className=" text-4xl font-bold pt-3">Peduli Sampah</p>
+        </div>
+      </div>
+      <Link href={"/auth/login"} className=" py-12 px-5 w-full">
+        <button className=" bg-white w-full rounded-full py-4 font-mulis font-bold text-lg text-[#308CFB]">
+          Lanjutkan
+        </button>
+      </Link>
+    </>
+  );
+};
+
+export default Sreen;
